504918bffc097e56f1efb0cbc0fae8b9008bd437
Omar Carrasco <omarcarrascoguerrero@gmail.com>
2024-10-23 16:48:41 -0500

    integracion de la libreria Google Guava, InmutableMap para class ProductoController

--- src/main/java/com/computec/computec/controller/HomeController.java
+++ src/main/java/com/computec/computec/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.computec.computec.controller;
 
 import com.computec.computec.service.IProductoService;
+import com.google.common.collect.ImmutableMap;
 import jakarta.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,13 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
 
-    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
 
     @Autowired
     private IProductoService productoService;
@@ -23,163 +25,37 @@ public class HomeController {
     public String home(Model model, HttpSession session) {
 
         //Logback
-        logger.info("sesión de usuario: {}", session.getAttribute("usuario"));
+        LOGGER.info("sesión de usuario: {}", session.getAttribute("usuario"));
 
         model.addAttribute("usuario", session.getAttribute("usuario"));
 
         return "usuario/inicio";
     }
 
-    @GetMapping("/procesador")
-    public String catProcesador(Model model, HttpSession session){
+    @GetMapping("/show/{categoria}")
+    public String catProcesador(@PathVariable String categoria, Model model, HttpSession session){
 
-        model.addAttribute("productos", productoService.findAllByCategoria("procesador"));
+        //Logback
+        LOGGER.info("Categoria enviada : {}", categoria);
+
+        // Google Guava
+        ImmutableMap<String, String> productosPorCategoria = ImmutableMap.of(
+                "procesador","procesador",
+                "ram","ram",
+                "ssd","ssd",
+                "hhd","hhd",
+                "tarjeta-madre","tmadre",
+                "tarjeta-grafica","tgrafica",
+                "disipador","disipador",
+                "fuente-poder","fpoder",
+                "case", "case"
+        );
+
+        model.addAttribute("productos", productoService.findAllByCategoria(productosPorCategoria.get(categoria)));
+
         model.addAttribute("usuario", session.getAttribute("usuario"));
-        model.addAttribute("c1", "c1");
-        model.addAttribute("c2", "");
-        model.addAttribute("c3", "");
-        model.addAttribute("c4", "");
-        model.addAttribute("c5", "");
-        model.addAttribute("c6", "");
-        model.addAttribute("c7", "");
-        model.addAttribute("c8", "");
-        model.addAttribute("c9", "");
 
         return "usuario/catalogo";
     }
-
-    @GetMapping("/ram")
-    public String showRam(Model model, HttpSession session){
-
-        model.addAttribute("productos", productoService.findAllByCategoria("ram"));
-        model.addAttribute("usuario", session.getAttribute("usuario"));
-        model.addAttribute("c1", "");
-        model.addAttribute("c2", "c2");
-        model.addAttribute("c3", "");
-        model.addAttribute("c4", "");
-        model.addAttribute("c5", "");
-        model.addAttribute("c6", "");
-        model.addAttribute("c7", "");
-        model.addAttribute("c8", "");
-        model.addAttribute("c9", "");
-        return "usuario/catalogo";
-    }
-
-    @GetMapping("/ssd")
-    public String showSSD(Model model, HttpSession session){
-
-        model.addAttribute("productos", productoService.findAllByCategoria("ssd"));
-        model.addAttribute("usuario", session.getAttribute("usuario"));
-        model.addAttribute("c1", "");
-        model.addAttribute("c2", "");
-        model.addAttribute("c3", "c3");
-        model.addAttribute("c4", "");
-        model.addAttribute("c5", "");
-        model.addAttribute("c6", "");
-        model.addAttribute("c7", "");
-        model.addAttribute("c8", "");
-        model.addAttribute("c9", "");
-
-        return "usuario/catalogo";
-    }
-
-    @GetMapping("/hhd")
-    public String showHHD(Model model, HttpSession session){
-
-        model.addAttribute("productos", productoService.findAllByCategoria("hhd"));
-        model.addAttribute("usuario", session.getAttribute("usuario"));
-        model.addAttribute("c1", "");
-        model.addAttribute("c2", "");
-        model.addAttribute("c3", "");
-        model.addAttribute("c4", "c4");
-        model.addAttribute("c5", "");
-        model.addAttribute("c6", "");
-        model.addAttribute("c7", "");
-        model.addAttribute("c8", "");
-        model.addAttribute("c9", "");
-        return "usuario/catalogo";
-    }
-
-    @GetMapping("/tarjeta-madre")
-    public String showTMadre(Model model, HttpSession session){
-        model.addAttribute("productos", productoService.findAllByCategoria("tmadre"));
-        model.addAttribute("usuario", session.getAttribute("usuario"));
-        model.addAttribute("c1", "");
-        model.addAttribute("c2", "");
-        model.addAttribute("c3", "");
-        model.addAttribute("c4", "");
-        model.addAttribute("c5", "c5");
-        model.addAttribute("c6", "");
-        model.addAttribute("c7", "");
-        model.addAttribute("c8", "");
-        model.addAttribute("c9", "");
-        return "usuario/catalogo";
-    }
-
-    @GetMapping("/tarjeta-grafica")
-    public String showTGrafica(Model model, HttpSession session){
-        model.addAttribute("productos", productoService.findAllByCategoria("tgrafica"));
-        model.addAttribute("usuario", session.getAttribute("usuario"));
-        model.addAttribute("c1", "");
-        model.addAttribute("c2", "");
-        model.addAttribute("c3", "");
-        model.addAttribute("c4", "");
-        model.addAttribute("c5", "");
-        model.addAttribute("c6", "c6");
-        model.addAttribute("c7", "");
-        model.addAttribute("c8", "");
-        model.addAttribute("c9", "");
-        return "usuario/catalogo";
-    }
-
-    @GetMapping("/disipador")
-    public String showDisipador(Model model, HttpSession session){
-        model.addAttribute("productos", productoService.findAllByCategoria("disipador"));
-        model.addAttribute("usuario", session.getAttribute("usuario"));
-        model.addAttribute("c1", "");
-        model.addAttribute("c2", "");
-        model.addAttribute("c3", "");
-        model.addAttribute("c4", "");
-        model.addAttribute("c5", "");
-        model.addAttribute("c6", "");
-        model.addAttribute("c7", "c7");
-        model.addAttribute("c8", "");
-        model.addAttribute("c9", "");
-        return "usuario/catalogo";
-    }
-
-    @GetMapping("/fuente-poder")
-    public String showFpoder(Model model, HttpSession session){
-        model.addAttribute("productos", productoService.findAllByCategoria("fpoder"));
-        model.addAttribute("usuario", session.getAttribute("usuario"));
-        model.addAttribute("c1", "");
-        model.addAttribute("c2", "");
-        model.addAttribute("c3", "");
-        model.addAttribute("c4", "");
-        model.addAttribute("c5", "");
-        model.addAttribute("c6", "");
-        model.addAttribute("c7", "");
-        model.addAttribute("c8", "c8");
-        model.addAttribute("c9", "");
-        return "usuario/catalogo";
-    }
-
-    @GetMapping("/case")
-    public String showCase(Model model, HttpSession session){
-        model.addAttribute("productos", productoService.findAllByCategoria("case"));
-        model.addAttribute("usuario", session.getAttribute("usuario"));
-        model.addAttribute("c1", "");
-        model.addAttribute("c2", "");
-        model.addAttribute("c3", "");
-        model.addAttribute("c4", "");
-        model.addAttribute("c5", "");
-        model.addAttribute("c6", "");
-        model.addAttribute("c7", "");
-        model.addAttribute("c8", "");
-        model.addAttribute("c9", "c9");
-        return "usuario/catalogo";
-    }
-
-
 
 }
